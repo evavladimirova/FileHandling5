a257546c44d66a3b9fe50c2a747b9dce153b533c
sport <sport@LAPTOP-61I2ACB0>
2023-10-02 12:22:32 +0300

    Add project files.

---
+++ FileHandling5/Program.cs
@@ -0,0 +1,39 @@
+﻿class Example
+{
+    public static void Main()
+    {
+        DirectoryInfo currDir = new DirectoryInfo(@"C:\Users\sport\Documents\ACS\Others\Copy");
+
+        currDir.Create();
+
+        string path = @"C:\Users\sport\Documents\ACS\Others\Copy\copy.txt";
+        string path2 = @"C:\Users\sport\Documents\ACS\Others\Copy\paste.txt";
+
+
+        try
+        {
+            if (!File.Exists(path))
+            {
+                using (FileStream fs = File.Create(path)) { }
+            }
+
+            if (File.Exists(path2))
+            {
+                File.Delete(path2);
+            }
+
+            File.Copy(path, path2);
+            Console.WriteLine($"File '{path}' has been copied and pasted to '{path2}'");
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+
+    }
+
+}
+
+
+
